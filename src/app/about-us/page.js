2b72d448cf9118eb55517683331739d999256718
farrawaaaay <farrahmaemagana@gmail.com>
2024-12-23 19:00:38 +0800

    about edit 12-23-24

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import "../../styles/about-us.css";
 
 export default function Home() {
   const router = useRouter();
@@ -24,25 +25,71 @@ export default function Home() {
             <Link href="/team">Team</Link>
             <Link href="/contact-us">Contact Us</Link>
             </div>
-            <div className="sign-buttons">
-            <button
-                onClick={() => router.push("/login")}
-                className="login-button w-full p-2 mt-4 bg-gray-200 text-gray-800 rounded hover:bg-gray-300"
-            >
-                Login
-            </button>
-            <button
-                onClick={() => router.push("/signup")}
-                className="signup-button w-full p-2 mt-4 bg-gray-200 text-gray-800 rounded hover:bg-gray-300"
-            >
-                Register
-            </button>
-            </div>
+            <h3 className="hide dots">.............................</h3>
 
 
         </div>
 
-        
+        <div className="about-main-section">
+            <div className="about-section">
+                <div className="picture-section">
+                    <Image
+                        src="/about-studying-1.svg"
+                        alt="writing A+"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <div className="details-section">
+                    <div className="top-details">
+                    <h1>We helped Students</h1>
+                    <p> reach their full potential</p>
+                    </div>
+                    <Link href="/signup" className="started">
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+            <div className="about-section reversed">
+                <div className="picture-section">
+                    <Image
+                        src="/studbud-logo.svg"
+                        alt="studbud logo"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <div className="details-section">
+                    <p><b style={{color:"#3C6E71"}}>StudBud</b> is a comprehensive academic productivity platform designed to help students reach their full potential. </p>
+                </div>
+            </div>
+            <div className="about-section">
+                <div className="picture-section">
+                    <Image
+                        src="/about-study-2.svg"
+                        alt="stressed studying"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <div className="details-section">
+                    <p>Our <i><b style={{color:"#3C6E71"}}>mission</b></i> is to empower students with the tools and resources they need to streamline their studies, boost their focus, and achieve their academic goals.</p>
+                </div>
+            </div>
+            <div className="about-section reversed">
+                <div className="picture-section">
+                    <Image
+                        src="/about-study-3.svg"
+                        alt="happy students"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <div className="details-section">
+                    <p style={{fontStyle:"italic", fontWeight:"bold", fontSize:"20px"}}> "Sometimes you just need a <b style={{color:"#8aafb1" }}>StudBud</b> to make your academic life easier."</p>
+                </div>
+            </div>
+        </div>
 
         <div className="footer">
             <div className="white-space">
