27d02c4f2183796853b46662f7197da09df89b71
Far <farrahmaemagana@gmail.com>
2025-01-10 02:58:35 +0800

    Update cloudinary.js

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -1,7 +1,1 @@
-import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
-  cloud_url: process.env.CLOUDINARY_URL, // Automatically parses the CLOUDINARY_URL
-});
-
-export default cloudinary;
